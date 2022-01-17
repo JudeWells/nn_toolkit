d09a0aaffcd789c3a5be39329a0ee6a01fab42fa
judewells <judewells@hotmail.com>
2022-01-17 13:57:47 +0000

    add plotting functions and network builder functions

---
+++ create_networks.py
@@ -0,0 +1,28 @@
+
+import keras
+from keras import layers
+
+def create_ffn(hidden_units, dropout_rate, name=None):
+    fnn_layers = []
+
+    for units in hidden_units:
+        fnn_layers.append(layers.BatchNormalization())
+        fnn_layers.append(layers.Dropout(dropout_rate))
+        fnn_layers.append(layers.Dense(units, activation=tf.nn.gelu))
+
+    return keras.Sequential(fnn_layers, name=name)
+
+
+
+def create_model_w_skiplayers(hidden_units, num_classes, dropout_rate=0.2):
+    inputs = layers.Input(shape=(num_features,), name="input_features")
+    x = create_ffn(hidden_units, dropout_rate, name=f"ffn_block1")(inputs)
+    for block_idx in range(4):
+        # Create an FFN block.
+        x1 = create_ffn(hidden_units, dropout_rate, name=f"ffn_block{block_idx + 2}")(x)
+        # Add skip connection.
+        x = layers.Add(name=f"skip_connection{block_idx + 2}")([x, x1])
+    # Compute logits.
+    logits = layers.Dense(num_classes, name="logits")(x)
+    # Create the model.
+    return keras.Model(inputs=inputs, outputs=logits, name="baseline")
---
+++ plotting_visualization.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def display_learning_curves(history):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+
+    ax1.plot(history.history["loss"])
+    ax1.plot(history.history["val_loss"])
+    ax1.legend(["train", "test"], loc="upper right")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+
+    ax2.plot(history.history["acc"])
+    ax2.plot(history.history["val_acc"])
+    ax2.legend(["train", "test"], loc="upper right")
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("Accuracy")
+    plt.show()
